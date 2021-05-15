2e7b92c54e40954796ed896117838c2c9ca68979
ErmiasTsege <71066745+ErmiasTsege@users.noreply.github.com>
2021-05-14 23:47:16 -0400

    ET:calculator Modular

--- src/com/qa/calculator/Calculator.java
+++ src/com/qa/calculator/Calculator.java
@@ -4,7 +4,17 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      
+		int resadd=Runner.add(2, 5);
+		System.out.println(resadd);
+		int ressub=Runner.sub(2, 5);
+		System.out.println(ressub);
+		int resmult=Runner.mult(2, 5);
+		System.out.println(resmult);
+		float resdiv=Runner.div(2, 5);
+		System.out.println(resdiv);
+		
 	}
+	
 
 }
