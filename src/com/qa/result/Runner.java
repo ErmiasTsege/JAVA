ed1476027403522fe515230d02eaf7c7077d3dc4
ErmiasTsege <71066745+ErmiasTsege@users.noreply.github.com>
2021-05-19 02:24:48 -0400

    ET:updated string,array,arraylis and classes

--- src/com/qa/result/Runner.java
+++ src/com/qa/result/Runner.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.qa.result;
 
 public class Runner {
@@ -19,26 +19,5 @@ public class Runner {
 
 }
 
-=======
-package com.qa.result;
 
-public class Runner {
-	
-	
-	
-	public static void main(String[] args) {
-		// Display students mark and overall percentage
-	     Result.displayResults();
-	  System.out.println("Overall Percentage:" + Result.overAllResult());
-		//Expand the Results class so that there is now a pass mark of 60%; if the person receives under 60% they get a fail message.
-	//Expand the previous example so that even if the person gets higher than 60% overall, if they fail 1 or more of the exams (pass grade of 60% for each exam) they still fail overall.
-		
-	  //Expand the above so that the message that is displayed varies depending on the number of subjects that they have failed
-	  Result.passFail();
-	  
-		
-	}
 
-}
-
->>>>>>> 77221b9b7992cb5cad46b6ab4ab5112a67562c56
