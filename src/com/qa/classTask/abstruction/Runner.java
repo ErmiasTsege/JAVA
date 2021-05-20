054496ac04b94fa46a3ed10626f89ea011494811
ErmiasTsege <71066745+ErmiasTsege@users.noreply.github.com>
2021-05-20 17:22:02 -0400

    ET:Inheritance and Abstruction

--- src/com/qa/classTask/abstruction/Runner.java
+++ src/com/qa/classTask/abstruction/Runner.java
@@ -5,7 +5,7 @@ public class Runner {
 	public static void main(String[] args) {
       Car sedan= new Car("Toyota","fusion", "2013", "white");
       System.out.println(sedan.getColor());
-  	
+      System.out.println(sedan.getDateOfCreation());
 	}
 
 }
